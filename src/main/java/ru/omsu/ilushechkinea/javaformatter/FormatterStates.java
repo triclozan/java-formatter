9dc656155f66a34608d6274740128e7d5b2e8e27
Ilyushechkin Eugeniy <triclozan@yandex.ru>
2013-11-02 23:16:39 +0700

    Fixed typo in comment

--- src/main/java/ru/omsu/ilushechkinea/javaformatter/FormatterStates.java
+++ src/main/java/ru/omsu/ilushechkinea/javaformatter/FormatterStates.java
@@ -5,7 +5,7 @@
 package ru.omsu.ilushechkinea.javaformatter;
 
 /**
- * Enumeration of finite automata (kernel of the formatter) states
+ * Enumeration of finite automaton (kernel of the formatter) states
  * @author ilushechkinea
  */
 public enum FormatterStates {
