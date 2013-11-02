d7a8b6156aa5e8091f427b91f2ccfcfb35c10295
Ilyushechkin Eugeniy <triclozan@yandex.ru>
2013-11-02 22:02:00 +0700

    Tests for null streams and for empty string handling

--- src/test/java/ru/omsu/ilushechkinea/javaformatter/JavaFormatterTest.java
+++ src/test/java/ru/omsu/ilushechkinea/javaformatter/JavaFormatterTest.java
@@ -19,49 +19,73 @@ import java.util.logging.Logger;
  * Unit test for simple App.
  */
 public class JavaFormatterTest {
-    String mockString;
-    ByteArrayOutputStream mockBaos;
+    private ByteArrayOutputStream mockBaos;
+    private Formatter formatter;
+    
     /*
      * Data initialization
      */
     @Before
     public void setUp() {
-        mockString = "mock";
         mockBaos = new ByteArrayOutputStream();
+        Formatter formatter = new Formatter();  
     }
     
-    @Test(expected=IOException.class)
-    @Ignore
-    public void testWrongInput() {        
-        //Formatter f = new Formatter(null, mockBaos);        
+    /*
+     * Test for null input stream
+     */    
+    @Test(expected=NullPointerException.class)
+    public void testNullInput() throws UnsupportedEncodingException, IOException {        
+        formatter.format(null, mockBaos);
     }
-    
+
+    /*
+     * Test for null output stream
+     */   
+    @Test(expected=NullPointerException.class)
+    public void testNullOutput() throws UnsupportedEncodingException, IOException {        
+        formatter.format(new StringInputStream("") , null);
+    }    
+
+    /*
+     * Tests for correct input and output streams (but input content can be wrong itself)
+     */     
     @Test
     public void testCorrectCases() 
             throws UnsupportedEncodingException, IOException 
     {
+        //Test for formatting of single string code
         String test1 = "qwe {int i=0; for (asdad) {sdf}}";
         String res1 = "qwe {\n" +
             "    int i=0; for (asdad) {\n" +
             "        sdf\n" +
             "    }\n" +
             "}";
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
         Formatter f = new Formatter();
-        f.format(new StringInputStream(test1), baos);
-        assertEquals(baos.toString(), res1);
+        f.format(new StringInputStream(test1), mockBaos);
+        assertEquals(mockBaos.toString(), res1);
 
+        //Test for preserving newlines during formatting
         String test2 = "qwe "
                 + "{int i=0; for (asdad) "
                 + "{sdf}"
                 + "}"; 
-        baos.reset();
-        f.format(new StringInputStream(test2), baos);
-        assertEquals(baos.toString(), res1);
+        mockBaos.reset();
+        f.format(new StringInputStream(test2), mockBaos);
+        assertEquals(mockBaos.toString(), res1);
         
-        String test3 = "{}}{{"; 
-        baos.reset();
-        f.format(new StringInputStream(test3), baos);
+        //Test for empty string handling
+        String test3 = ""; 
+        String res2 = "";
+        mockBaos.reset();
+        f.format(new StringInputStream(test3), mockBaos);
+        assertEquals(mockBaos.toString(), res2);
+        
+        //Test for brace mismatch detection
+        String test10 = "{}}{{"; 
+        mockBaos.reset();
+        f.format(new StringInputStream(test10), mockBaos);
         FormatterWarningInfo wi = new FormatterWarningInfo(FormatterWarnings.WRN_LEFT_BRACE, 2, -1, -1);
         FormatterWarningInfo wi2 = new FormatterWarningInfo(FormatterWarnings.WRN_RIGHT_BRACE, 1, -1, -1);
         assertEquals(f.getWarnings().contains(wi), true);
