7374c7583884b58f94925d90cdd76e133ff1e6b1
Ilyushechkin Eugeniy <triclozan@yandex.ru>
2013-11-12 16:56:09 +0700

    Splitted correct test cases

--- src/test/java/ru/omsu/ilushechkinea/javaformatter/JavaFormatterTest.java
+++ src/test/java/ru/omsu/ilushechkinea/javaformatter/JavaFormatterTest.java
@@ -60,57 +60,91 @@ public class JavaFormatterTest {
     }   
     
     /*
-     * Tests for correct input and output streams (but input content can be wrong itself)
+     * Test for formatting of single string code
      */     
     @Test
-    public void testCorrectCases() 
+    public void testSingleLine()
              throws InvalidStreamException, 
              FormattingException,
              UnsupportedEncodingException {
-        //Test for formatting of single string code
         String test1 = "qwe {int i=0; for (asdad) {sdf}}";
         String res1 = "qwe {\n" +
-            "    int i = 0;\n" + 
+            "    int i = 0;\n" +
             "    for (asdad) {\n" +
             "        sdf\n" +
             "    }\n" +
             "}";
         formatter.format(new StringInputStream(test1), mockBaos);
         assertEquals(mockBaos.toString(), res1);
+    }
 
-        //Test for preserving newlines during formatting
-        String test2 = "qwe "
+    /*
+     * Test for preserving line breaks during formatting
+     */
+    @Test
+    public void testLineBreaks()
+            throws InvalidStreamException,
+            FormattingException,
+            UnsupportedEncodingException {
+        String test = "qwe "
                 + "{int i = 0; for (asdad) "
                 + "{sdf}"
-                + "}"; 
-        mockBaos.reset();
-        formatter.format(new StringInputStream(test2), mockBaos);
-        assertEquals(mockBaos.toString(), res1);
-        
-        //Test for empty string handling
-        String test3 = ""; 
-        String res2 = "";
-        mockBaos.reset();
-        formatter.format(new StringInputStream(test3), mockBaos);
-        assertEquals(mockBaos.toString(), res2);
-        
-        //Test for string literals and operation signs handling
-        String test4 = "{i =\"{}\";for(a =1){b =2+5;c =7}}";
-        String res3 = "{\n" +
-                    "    i = \"{}\";\n" +
-                    "    for(a = 1){\n" +
-                    "        b = 2 + 5;\n" +
-                    "        c = 7\n" +
-                    "    }\n" +
-                    "}";
-        mockBaos.reset();
-        formatter.format(new StringInputStream(test4), mockBaos);
-        assertEquals(mockBaos.toString(), res3);
-        
-        //Test for brace mismatch detection
-        String test10 = "{}}{{"; 
-        mockBaos.reset();
-        formatter.format(new StringInputStream(test10), mockBaos);
+                + "}";
+        String res = "qwe {\n" +
+                "    int i = 0;\n" +
+                "    for (asdad) {\n" +
+                "        sdf\n" +
+                "    }\n" +
+                "}";
+
+        formatter.format(new StringInputStream(test), mockBaos);
+        assertEquals(mockBaos.toString(), res);
+    }
+
+    /*
+     * Test for empty string handling
+     */
+    @Test
+    public void testEmptyString()
+            throws InvalidStreamException,
+            FormattingException,
+            UnsupportedEncodingException {
+        String test = "";
+        String res = "";
+        formatter.format(new StringInputStream(test), mockBaos);
+        assertEquals(mockBaos.toString(), res);
+    }
+
+    /*
+     * Test for string literals and operation signs handling
+     */
+    @Test
+    public void testLiteralsOperations()
+            throws InvalidStreamException,
+            FormattingException,
+            UnsupportedEncodingException {
+        String test = "{i =\"{}\";for(a =1){b =2+5;c =7}}";
+        String res = "{\n" +
+                "    i = \"{}\";\n" +
+                "    for(a = 1){\n" +
+                "        b = 2 + 5;\n" +
+                "        c = 7\n" +
+                "    }\n" +
+                "}";
+        formatter.format(new StringInputStream(test), mockBaos);
+        assertEquals(mockBaos.toString(), res);
+    }
+
+    /*
+     * Test for brace mismatch detection
+     */
+    @Test
+    public void testBraceMismatchDetection()
+            throws InvalidStreamException,
+            FormattingException,
+            UnsupportedEncodingException {
+        String test = "{}}{{";
+        formatter.format(new StringInputStream(test), mockBaos);
         FormatterWarningInfo wi = new FormatterWarningInfo(FormatterWarnings.WRN_LEFT_BRACE, 2, -1, -1);
         FormatterWarningInfo wi2 = new FormatterWarningInfo(FormatterWarnings.WRN_RIGHT_BRACE, 1, -1, -1);
         assertEquals(formatter.getWarnings().contains(wi), true);
